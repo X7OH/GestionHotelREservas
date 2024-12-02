08cfa6269d835cbcc81a8db94856682267093aa2
X7OH <allandavid184@gmail.com>
2024-12-02 08:04:45 -0500

    Actualicé admin.py para poder ver los usuarios en el panel de Django

--- Hotel/HotelApp/admin.py
+++ Hotel/HotelApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario
 
-# Register your models here.
+admin.site.register(Usuario)
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'correo', 'Ciudad', 'rol')  # Campos visibles en la lista
+    search_fields = ('nombre', 'correo')  # Habilita búsqueda por estos campos
+    list_filter = ('rol', 'Ciudad')  # Habilita filtros por rol y Ciudad
+
+admin.site.register(Usuario, UsuarioAdmin)
