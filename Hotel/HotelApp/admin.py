dddf1aa2b62b5c14dc055565f03c85ff301d247e
X7OH <allandavid184@gmail.com>
2024-12-02 08:07:00 -0500

    Actualicé admin.py para poder ver las reservas en el panel de Django

--- Hotel/HotelApp/admin.py
+++ Hotel/HotelApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, Reservas, Habitacion
 
 admin.site.register(Usuario)
 
@@ -9,3 +9,11 @@ class UsuarioAdmin(admin.ModelAdmin):
     list_filter = ('rol', 'Ciudad')  # Habilita filtros por rol y Ciudad
 
 admin.site.register(Usuario, UsuarioAdmin)
+
+admin.site.register(Reservas)
+class ReservasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'correo', 'piso', 'nhabitacion')  # Campos visibles en la lista
+    search_fields = ('nombre', 'correo')  # Habilitar búsqueda
+    list_filter = ('piso',)  # Filtros por piso
+
+admin.site.register(Reservas, ReservasAdmin)
