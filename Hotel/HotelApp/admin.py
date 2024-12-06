45324bf1049fa9e2cb58e80d6ee175531a7bcc3e
X7OH <allandavid184@gmail.com>
2024-12-06 07:25:42 -0500

    Actualizaciones varias

--- Hotel/HotelApp/admin.py
+++ Hotel/HotelApp/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Usuario, Reservas, Habitacion
 
-admin.site.register(Usuario)
-
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'correo', 'Ciudad', 'rol')  # Campos visibles en la lista
     search_fields = ('nombre', 'correo')  # Habilita búsqueda por estos campos
@@ -10,7 +8,6 @@ class UsuarioAdmin(admin.ModelAdmin):
 
 admin.site.register(Usuario, UsuarioAdmin)
 
-admin.site.register(Reservas)
 class ReservasAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'correo', 'piso', 'nhabitacion')  # Campos visibles en la lista
     search_fields = ('nombre', 'correo')  # Habilitar búsqueda
@@ -18,7 +15,6 @@ class ReservasAdmin(admin.ModelAdmin):
 
 admin.site.register(Reservas, ReservasAdmin)
 
-admin.site.register(Habitacion)
 class HabitacionAdmin(admin.ModelAdmin):
     list_display = ('id', 'piso', 'nhabitacion', 'Estado')  # Campos visibles en la lista
     search_fields = ('nhabitacion',)  # Búsqueda por número de habitación
